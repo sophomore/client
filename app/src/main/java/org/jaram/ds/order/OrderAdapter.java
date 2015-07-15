72151df9b6662dd86309f8a5b76a25cdbf9cfca7
Sun Yeong <vmffkxlgnqh1@gmail.com>
2015-07-15 12:20:34 +0900

    결제단계 대략의 레이아웃

---
+++ app/src/main/java/org/jaram/ds/order/OrderAdapter.java
@@ -0,0 +1,50 @@
+package org.jaram.ds.order;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by vmffkxlgnqh1 on 15. 7. 12..
+ */
+public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder> {
+
+    private List<Order_object> mOrderList;
+
+    public OrderAdapter(List<Order_object> list){
+        mOrderList = list;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.payment_orderitem_layout,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.menuName.setText(mOrderList.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mOrderList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView menuName;
+
+        public ViewHolder(View itemView){
+
+            super(itemView);
+
+            menuName = (TextView)itemView.findViewById(R.id.menuname);
+
+        }
+
+    }
+}
