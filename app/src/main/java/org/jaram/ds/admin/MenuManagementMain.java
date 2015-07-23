6609076127f7b7450c71425f22a7d546f30d412e
김명수 <5qndjddl6@naver.com>
2015-07-23 10:41:58 +0900

    test

---
+++ app/src/main/java/org/jaram/ds/admin/MenuManagementMain.java
@@ -0,0 +1,28 @@
+package org.jaram.ds.admin;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import org.jaram.ds.R;
+
+/**
+ * Created by KimMyoungSoo on 2015. 7. 15..
+ */
+public class MenuManagementMain extends Fragment{
+    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+        View view = inflater.inflate(R.layout.fragment_menu_management, container, false);
+
+        ListView listView = (ListView) view.findViewById(R.id.)
+
+
+
+
+        return view;
+    }
+
+}
---
+++ app/src/main/java/org/jaram/ds/admin/view/MenuListFrag.java
@@ -0,0 +1,8 @@
+package org.jaram.ds.admin.view;
+
+/**
+ * Created by KimMyoungSoo on 2015. 7. 15..
+ */
+public class MenuListFrag {
+
+}
