bac1bb3794eb3298a4030ba36ed5561eab075ed3
김명수 <5qndjddl6@naver.com>
2015-08-11 23:53:52 +0900

    CategoryManagement 추

---
+++ app/src/main/java/org/jaram/ds/admin/CategoryMenagement.java
@@ -0,0 +1,17 @@
+package org.jaram.ds.admin;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import org.jaram.ds.R;
+
+/**
+ * Created by KimMyoungSoo on 2015. 8. 11..
+ */
+public class CategoryMenagement extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
