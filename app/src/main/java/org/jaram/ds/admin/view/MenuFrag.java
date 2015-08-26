0a7bcc3d74e863094e64dd7a2257e54bba01c97d
김명수 <5qndjddl6@naver.com>
2015-08-27 05:17:00 +0900

    메뉴 수정//메뉴 추가//메뉴 삭제//

--- app/src/main/java/org/jaram/ds/admin/view/MenuFrag.java
+++ app/src/main/java/org/jaram/ds/admin/view/MenuFrag.java
@@ -1,27 +1,101 @@
 package org.jaram.ds.admin.view;
 
-import android.app.Fragment;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
 
 import org.jaram.ds.R;
+import org.jaram.ds.data.Data;
+import org.jaram.ds.data.struct.Category;
+import org.jaram.ds.data.struct.Menu;
+
+import java.util.ArrayList;
 
 /**
  * Created by KimMyoungSoo on 2015. 7. 15..
  */
-public class MenuFrag extends Fragment{
-    public MenuFrag (){
-
+public class MenuFrag extends Fragment {
+    ArrayList<Menu> menuList;
+    MenuListAdapter adapter;
+    public MenuFrag() {
+        menuList = Data.menuList;
     }
-
+    public void setAdapter(MenuListAdapter adapter){
+        this.adapter = adapter;
+    }
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_menu_management,container,true);
+        View view = inflater.inflate(R.layout.fragment_menu_management,container,false);
+        View liview = inflater.inflate(R.layout.fragment_menu_list,container,false);
+        final View addView = inflater.inflate(R.layout.fragment_menu_add,container,false);
+
+        final String[] categorys = {"category 1","category 2","category 3","category 4","category 5"};
+
+        final ListView listview = (ListView)liview.findViewById(R.id.listView);
+        listview.setAdapter(adapter);
+
+
+        Button addButton = (Button) view.findViewById(R.id.addmenu);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Spinner categorySpinner;
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                final String[] categorys = {"category 1","category 2","category 3","category 4","category 5"};
+
+                final View addView = inflater.inflate(R.layout.fragment_menu_add, container,false);
+                categorySpinner = (Spinner) addView.findViewById(R.id.cateSpinner);
+
+                final ArrayAdapter<String> spiAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, categorys);
+                spiAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                categorySpinner.setAdapter(spiAdapter);
+
+                builder.setTitle("메뉴 추가");
+                builder.setView(addView);
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        EditText menuText = (EditText) addView.findViewById(R.id.menuAdd);
+                        EditText priceText = (EditText) addView.findViewById(R.id.priceAdd);
+
+                        final String menu = String.valueOf(menuText.getText());
+                        int price = Integer.parseInt(String.valueOf(priceText.getText()));
+                        final ArrayList<Category> category = Data.categoryList;
+                        final Menu temp = new Menu();
+
+                        temp._id = menuList.size();
+                        temp.price = price;
+                        temp.name = menu;
+                        temp.category = category.get(categorySpinner.getSelectedItemPosition());
+                        menuList.add(temp);
+
+
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.create().show();
+
+            }
+        });
 
         return view;
     }
--- app/src/main/java/org/jaram/ds/admin/view/MenuListFrag.java
+++ app/src/main/java/org/jaram/ds/admin/view/MenuListFrag.java
@@ -1,16 +1,18 @@
 package org.jaram.ds.admin.view;
 
 import android.app.AlertDialog;
-import android.app.Fragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import org.jaram.ds.R;
@@ -22,12 +24,16 @@ import java.util.ArrayList;
 /**
  * Created by KimMyoungSoo on 2015. 7. 15..
  */
-public class MenuListFrag extends Fragment{
+public class MenuListFrag extends Fragment {
 
     ArrayList<Menu> menuList;
-
+    MenuListAdapter adapter;
     public MenuListFrag() {
+
         menuList = Data.menuList;
+    }
+    public void setAdapter(MenuListAdapter adapter){
+        this.adapter = adapter;
     }
 
     @Nullable
@@ -35,15 +41,24 @@ public class MenuListFrag extends Fragment{
     public View onCreateView(final LayoutInflater inflater,  final ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_menu_list,container,false);
-        final MenuListAdapter adapter = new MenuListAdapter(this.getActivity());
+        final String[] categorys = {"category 1","category 2","category 3","category 4","category 5"};
+
         ListView listView = (ListView) view.findViewById(R.id.listView);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, final View view, final int position, long id) {
-
+                final Spinner categorySpinner;
                 AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                final View dialogView = inflater.inflate(R.layout.fragment_menu_dialog, container);
+
+                final View dialogView = inflater.inflate(R.layout.fragment_menu_dialog, container,false);
+                categorySpinner = (Spinner) dialogView.findViewById(R.id.categorySpinner);
+
+                ArrayAdapter<String> spiAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, categorys);
+                spiAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                categorySpinner.setAdapter(spiAdapter);
+
                 builder.setTitle("메뉴 수정");
                 builder.setView(dialogView);
                 builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
@@ -51,19 +66,15 @@ public class MenuListFrag extends Fragment{
                     public void onClick(DialogInterface dialog, int which) {
                         EditText nameText = (EditText) dialogView.findViewById(R.id.menuModify);
                         EditText priceText = (EditText) dialogView.findViewById(R.id.priceModify);
-                        EditText categoryText = (EditText) dialogView.findViewById(R.id.categoryModify);
-
-                        String menu = nameText.getText().toString();
-                        int price = Integer.parseInt(priceText.getText().toString());
-                        String category = categoryText.getText().toString();
+                        final String menu = String.valueOf(nameText.getText());
+                        int price = Integer.parseInt(String.valueOf(priceText.getText()));
 
                         menuList.get(position)._id = position;
                         menuList.get(position).name = menu;
                         menuList.get(position).price = price;
-                        menuList.get(position).category.name = category;
+                        menuList.get(position).category.name = String.valueOf(categorySpinner.getSelectedItem());;
 
                         adapter.notifyDataSetChanged();
-
 
                     }
                 }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -77,13 +88,16 @@ public class MenuListFrag extends Fragment{
         });
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
                 builder.setTitle("삭제 하시겠습니까?");
                 builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         Toast.makeText(getActivity(), "deleted", Toast.LENGTH_LONG).show();
+                        menuList.remove(position);
+
+                        adapter.notifyDataSetChanged();
                     }
                 });
                 builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
