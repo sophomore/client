d1df21a6f72ee0367d519bc5011e38fcdca85e75
JinYong Kim <jinyong@jadekim.kr>
2015-08-27 06:42:28 +0900

    메뉴 관리화면 레이아웃 변

--- app/src/main/java/org/jaram/ds/admin/MenuManagementMain.java
+++ app/src/main/java/org/jaram/ds/admin/MenuManagementMain.java
@@ -6,13 +6,22 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.jaram.ds.R;
+import org.jaram.ds.data.Data;
+import org.jaram.ds.data.struct.Menu;
+import org.jaram.ds.data.struct.OrderMenu;
+
+import java.util.ArrayList;
 
 /**
  * Created by KimMyoungSoo on 2015. 7. 15..
@@ -24,58 +33,54 @@ public class MenuManagementMain extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu_management);
+        setContentView(R.layout.activity_menu_management2);
 
-        Button deletebutton = (Button) findViewById(R.id.deletemenu);
-        deletebutton.setOnClickListener(new View.OnClickListener() {
 
-            public void onClick(View v) {
 
-                Builder builder = new Builder(MenuManagementMain.this);
-                builder.setMessage("삭제 하시겠습니까?")
-                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Toast.makeText(getApplicationContext(), "삭제되었습니다", Toast.LENGTH_LONG).show();
-                            }
-                        }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-                builder.create().show();
+        ArrayList<Menu> menuBtns = new ArrayList<Menu>();
+        menuBtns.addAll(Data.menuList);
+        RecyclerView menuBtnListViewDon = (RecyclerView)findViewById(R.id.DonMenuList);
+        MenuSelectBtnAdapter menuBtnAdapterDon = new MenuSelectBtnAdapter(menuBtns);
+        menuBtnListViewDon.setAdapter(menuBtnAdapterDon);
+        menuBtnListViewDon.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
 
-            }
-        });
+        RecyclerView menuBtnListViewDup = (RecyclerView)findViewById(R.id.DupMenuList);
+        MenuSelectBtnAdapter menuBtnAdapterDup = new MenuSelectBtnAdapter(menuBtns);
+        menuBtnListViewDup.setAdapter(menuBtnAdapterDup);
+        menuBtnListViewDup.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
 
-        Button categorybutton = (Button) findViewById(R.id.categorizemenu);
-        categorybutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MenuManagementMain.this, CategoryMenagement.class));
-            }
-        });
+        RecyclerView menuBtnListViewNoodle = (RecyclerView)findViewById(R.id.NoodleMenuList);
+        MenuSelectBtnAdapter menuBtnAdapterNoodle = new MenuSelectBtnAdapter(menuBtns);
+        menuBtnListViewNoodle.setAdapter(menuBtnAdapterNoodle);
+        menuBtnListViewNoodle.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
 
-        Button modifybutton = (Button) findViewById(R.id.modifymenu);
-        modifybutton.setOnClickListener(new View.OnClickListener() {
+        RecyclerView menuBtnListViewLast = (RecyclerView)findViewById(R.id.DrinkAndAdd);
+        MenuSelectBtnAdapter menuBtnAdapterLast = new MenuSelectBtnAdapter(menuBtns);
+        menuBtnListViewLast.setAdapter(menuBtnAdapterLast);
+        menuBtnListViewLast.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
+
+
+        TextView don = (TextView)findViewById(R.id.DonGgas);
+        don.setOnClickListener(new View.OnClickListener() {
 
             Context mcontext = getApplicationContext();
             LayoutInflater inflater = (LayoutInflater) mcontext.getSystemService(LAYOUT_INFLATER_SERVICE);
-            View layout = inflater.inflate(R.layout.fragment_menu_dialog, (ViewGroup) findViewById(R.id.layout_root));
+            View layout = inflater.inflate(R.layout.fragment_menu_add, (ViewGroup) findViewById(R.id.layout_root_add));
+
+            Builder builder = new Builder(MenuManagementMain.this);
 
             @Override
             public void onClick(View v) {
-                Builder builder = new Builder(MenuManagementMain.this);
-                builder.setTitle("메뉴 수정");
+                builder.setTitle("메뉴 추가");
                 builder.setView(layout);
 
                 builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(getApplicationContext(), "수정되었습니다.", Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), "추가되었습니다.", Toast.LENGTH_LONG).show();
                     }
                 });
+
                 builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -84,12 +89,41 @@ public class MenuManagementMain extends FragmentActivity {
                 });
                 builder.create().show();
 
+            }
+        });
+        TextView dup = (TextView)findViewById(R.id.DupBbap);
+        dup.setOnClickListener(new View.OnClickListener() {
+
+            Context mcontext = getApplicationContext();
+            LayoutInflater inflater = (LayoutInflater) mcontext.getSystemService(LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.fragment_menu_add, (ViewGroup) findViewById(R.id.layout_root_add));
+
+            Builder builder = new Builder(MenuManagementMain.this);
+
+            @Override
+            public void onClick(View v) {
+                builder.setTitle("메뉴 추가");
+                builder.setView(layout);
+
+                builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "추가되었습니다.", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.create().show();
 
             }
         });
-
-        Button addbutton = (Button) findViewById(R.id.addmenu);
-        addbutton.setOnClickListener(new View.OnClickListener() {
+        TextView noo = (TextView)findViewById(R.id.Noodles);
+        noo.setOnClickListener(new View.OnClickListener() {
 
             Context mcontext = getApplicationContext();
             LayoutInflater inflater = (LayoutInflater) mcontext.getSystemService(LAYOUT_INFLATER_SERVICE);
@@ -120,7 +154,204 @@ public class MenuManagementMain extends FragmentActivity {
             }
         });
 
+        TextView drink = (TextView)findViewById(R.id.DrinkandSpecial);
+        drink.setOnClickListener(new View.OnClickListener() {
+
+            Context mcontext = getApplicationContext();
+            LayoutInflater inflater = (LayoutInflater) mcontext.getSystemService(LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.fragment_menu_add, (ViewGroup) findViewById(R.id.layout_root_add));
+
+            Builder builder = new Builder(MenuManagementMain.this);
+
+            @Override
+            public void onClick(View v) {
+                builder.setTitle("메뉴 추가");
+                builder.setView(layout);
+
+                builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "추가되었습니다.", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.create().show();
+
+            }
+        });
+
+
+
+
+//        setContentView(R.layout.activity_menu_management);
+//
+//        Button deletebutton = (Button) findViewById(R.id.deletemenu);
+//        deletebutton.setOnClickListener(new View.OnClickListener() {
+//
+//            public void onClick(View v) {
+//
+//                Builder builder = new Builder(MenuManagementMain.this);
+//                builder.setMessage("삭제 하시겠습니까?")
+//                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                Toast.makeText(getApplicationContext(), "삭제되었습니다", Toast.LENGTH_LONG).show();
+//                            }
+//                        }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.cancel();
+//                    }
+//                });
+//                builder.create().show();
+//
+//            }
+//        });
+//
+//        Button categorybutton = (Button) findViewById(R.id.categorizemenu);
+//        categorybutton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(MenuManagementMain.this, CategoryMenagement.class));
+//            }
+//        });
+//
+//        Button modifybutton = (Button) findViewById(R.id.modifymenu);
+//        modifybutton.setOnClickListener(new View.OnClickListener() {
+//
+//            Context mcontext = getApplicationContext();
+//            LayoutInflater inflater = (LayoutInflater) mcontext.getSystemService(LAYOUT_INFLATER_SERVICE);
+//            View layout = inflater.inflate(R.layout.fragment_menu_dialog, (ViewGroup) findViewById(R.id.layout_root));
+//
+//            @Override
+//            public void onClick(View v) {
+//                Builder builder = new Builder(MenuManagementMain.this);
+//                builder.setTitle("메뉴 수정");
+//                builder.setView(layout);
+//
+//                builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        Toast.makeText(getApplicationContext(), "수정되었습니다.", Toast.LENGTH_LONG).show();
+//                    }
+//                });
+//                builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.cancel();
+//                    }
+//                });
+//                builder.create().show();
+//
+//
+//            }
+//        });
+//
+//        Button addbutton = (Button) findViewById(R.id.addmenu);
+//        addbutton.setOnClickListener(new View.OnClickListener() {
+//
+//            Context mcontext = getApplicationContext();
+//            LayoutInflater inflater = (LayoutInflater) mcontext.getSystemService(LAYOUT_INFLATER_SERVICE);
+//            View layout = inflater.inflate(R.layout.fragment_menu_add, (ViewGroup) findViewById(R.id.layout_root_add));
+//
+//            Builder builder = new Builder(MenuManagementMain.this);
+//
+//            @Override
+//            public void onClick(View v) {
+//                builder.setTitle("메뉴 추가");
+//                builder.setView(layout);
+//
+//                builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        Toast.makeText(getApplicationContext(), "추가되었습니다.", Toast.LENGTH_LONG).show();
+//                    }
+//                });
+//
+//                builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.cancel();
+//                    }
+//                });
+//                builder.create().show();
+//
+//            }
+//        });
+
     }
 
+    private class MenuSelectBtnAdapter extends RecyclerView.Adapter<MenuSelectBtnAdapter.MenuSelectBtnViewHolder> {
 
+        ArrayList<Menu> menuList = null;
+        public MenuSelectBtnAdapter(ArrayList<Menu> menuList) {
+            this.menuList = menuList;
+        }
+
+
+        @Override
+        public MenuSelectBtnViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new MenuSelectBtnViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.menubtn_item, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(MenuSelectBtnViewHolder holder, int position) {
+            final int i =position;
+
+            holder.name.setText(menuList.get(position).name);
+            holder.price.setText(menuList.get(position).price+"");
+        }
+
+        @Override
+        public int getItemCount() {
+            return menuList.size();
+        }
+
+        public class MenuSelectBtnViewHolder extends RecyclerView.ViewHolder {
+
+            public TextView name;
+            public TextView price;
+            public MenuSelectBtnViewHolder(View item) {
+                super(item);
+                price = (TextView)item.findViewById(R.id.PriceOfMenu);
+
+                name = (TextView)item.findViewById(R.id.NameOfMenu);
+
+                item.setOnClickListener(new View.OnClickListener() {
+
+                    Context mcontext = getApplicationContext();
+                    LayoutInflater inflater = (LayoutInflater) mcontext.getSystemService(LAYOUT_INFLATER_SERVICE);
+                    View layout = inflater.inflate(R.layout.fragment_menu_dialog, (ViewGroup) findViewById(R.id.layout_root));
+
+                    @Override
+                    public void onClick(View view) {
+
+                        Builder builder = new Builder(MenuManagementMain.this);
+                        builder.setTitle("메뉴 수정");
+                        builder.setView(layout);
+
+                        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(getApplicationContext(), "수정되었습니다.", Toast.LENGTH_LONG).show();
+                            }
+                        });
+                        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                        builder.create().show();
+                    }
+                });
+            }
+        }
+    }
 }
